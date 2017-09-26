98f93d59dee46e93f1d843ae5443c4f495212cfc
unnamedsid <sidhant.r60@gmail.com>
2017-09-26 23:41:06 +0530

    Fixes localStorage problem

--- popup.js
+++ popup.js
@@ -1,28 +1,39 @@
 var app = angular.module('tabHistoryApp', []);
 
-app.controller('tabHistoryController', function($scope) {
-	var listUrl = [];
+app.controller('tabHistoryController', function($scope, localStorageService, $rootScope) {
+	var listUrl;
+	if(window.localStorage.getItem("url")=="null")
+		listUrl = [];
+	else
+		listUrl = JSON.parse(window.localStorage.getItem("url"));
+
+	// Addition of new url
 	$scope.submit = function () {
-		$scope.url = $scope.url;
 		listUrl.unshift($scope.url);
-		chrome.storage.sync.set({'url': listUrl})
+		window.localStorage.setItem("url", JSON.stringify(listUrl));
 		$scope.getAllUrls();
 	}
+
+	// Remove an url
 	$scope.remove = function(url) {
 		var index = listUrl.indexOf(url);
 		listUrl.splice(index,1);
-		chrome.storage.sync.set({'url': listUrl})
+		window.localStorage.setItem("url", listUrl);
 		$scope.getAllUrls();
 	}
+
+	// on page onload
 	$scope.promises = [];
+
+	// Get all urls
 	$scope.getAllUrls = function () {
-		chrome.storage.sync.get("url", function(data) {
-			$scope.listUrl = data['url'];
-		})
+		$scope.listUrl=JSON.parse(window.localStorage.getItem("url"));
 	}
+
+	// Open each link in a new tab
 	$scope.open = function () {
-		listUrl.forEach(function (elem) {
-			window.open(elem, '');
+		$scope.listUrl.forEach(function (elem) {
+			window.open(elem,'');
 		})
 	}
 	$scope.promises.push($scope.getAllUrls());
